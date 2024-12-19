b64e5c0b232b62e343b57307970f3acf41119d1d
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2024-12-19 11:37:55 +0800

    update crewai demo

--- crewai_demo/run_crewai.py
+++ crewai_demo/run_crewai.py
@@ -2,5 +2,6 @@ import os
 
 if __name__ == '__main__':
     os.environ['VIRTUAL_ENV'] = './.venv'
-    print('crewai starts.')
+    print('CrewAI project starts.')
+    os.system('crewai install')
     os.system('crewai run')
