8d4caa68a7794d7753483515f852aa66ce397c24
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2024-12-19 10:46:05 +0800

    ceo multiagent demo

--- ceo_demo.py
+++ ceo_demo.py
@@ -1,17 +1,18 @@
 import logging
 
+from sympy import simplify
+from dotenv import load_dotenv
 from ceo import (
     Agent,
     Personality,
     get_openai_model,
+    agentic,
     ability
 )
-from sympy import simplify
-from dotenv import load_dotenv
 
 load_dotenv()
 logging.getLogger('ceo').setLevel(logging.DEBUG)
-# logging.getLogger('ceo.prompt').setLevel(logging.DEBUG)
+model = get_openai_model()
 
 
 @ability
@@ -27,8 +28,22 @@ def write_file(filename: str, content: str) -> str:
     return f'{content} written to {filename}.'
 
 
+jack = Agent(abilities=[calculator], brain=model, name='Jack', personality=Personality.INQUISITIVE)
+tylor = Agent(abilities=[write_file], brain=model, name='Tylor', personality=Personality.PRUDENT)
+
+
+@agentic(jack)
+def agent1():
+    return
+
+
+@agentic(tylor)
+def agent2():
+    return
+
+
 if __name__ == '__main__':
-    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='CEO', personality=Personality.INQUISITIVE)
-    ceo.assign(r"这是一个半径为 (1 * 9.5 / 2 * 2) 厘米的球体，其中π取值为 3.14159，请分别计算其表面积和体积，然后将结果写入 D:\project\playground\result.txt，")
+    ceo = Agent(abilities=[agent1, agent2], brain=model, name='CEO', personality=Personality.INQUISITIVE)
+    ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
